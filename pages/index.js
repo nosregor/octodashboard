4e01837c6d48db8dcbdaa4faefae2e52c993dbdd
Andrew Rogerson <andrew.rogerson@free-now.com>
2021-02-02 09:42:03 +0100

    fix linter

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,32 @@
-import React, { useState } from 'react';
-import Router from 'next/router';
-import Octicon, { MarkGithub, Search } from '@primer/octicons-react';
-import { Head, Footer } from '../components';
+import React, { useState } from "react";
+import Router from "next/router";
+import Octicon, { MarkGithub } from "@primer/octicons-react";
+import { Head } from "../components";
 
-import styled from 'styled-components';
-import { theme, mixins } from '../style';
+import styled from "styled-components";
+import { theme, mixins } from "../style";
 
 const { colors, fonts } = theme;
 
 const StyledContainer = styled.div`
   ${mixins.flexCenter};
-  background-color: ${colors.white};
-  // background-image: linear-gradient(${colors.black} 0%, ${colors.darkGrey} 100%);
-  color: ${colors.solstice};
+  background-color: ${colors.black};
+  background-image: linear-gradient(
+    ${colors.black} 0%,
+    ${colors.darkGrey} 100%
+  );
+  color: ${colors.offWhite};
   height: 100vh;
 
   form {
     background-color: transparent;
     border-radius: 5px;
-    padding: 2rem;
+    // padding: 2rem;
     margin-bottom: 20vh;
     max-width: 600px;
     text-align: center;
     svg {
-      color: ${colors.nebula};
+      color: ${colors.blue};
     }
     label {
       display: block;
@@ -32,20 +35,25 @@ const StyledContainer = styled.div`
       margin: 2rem;
     }
     input {
-      background-color: ${colors.moon};
+      background-color: #26303c;
       outline: 0;
       border: 0;
       border-radius: 0.25rem;
       width: 100%;
       max-width: 500px;
       margin: 0 auto;
-      padding: 1.4rem;
-      // color: ${colors.lightblue};
-      color: ${colors.solstice};
+      padding: 1rem;
+      color: ${colors.lightblue};
       font-family: ${fonts.mono};
-      font-size: 1.4rem;
+      font-size: 2rem;
       font-weight: 400;
-      text-align: left;
+      text-align: center;
+    }
+    p {
+      color: ${colors.lightblue};
+      font-family: ${fonts.mono};
+      font-size: 1rem;
+      font-weight: 400;
     }
 
     .submit {
@@ -58,7 +66,7 @@ const StyledContainer = styled.div`
 
 // using react hooks, refer to: https://flaviocopes.com/react-hooks/
 const Home = () => {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   const handleChange = event => setUsername(event.target.value);
 
   return (
@@ -70,14 +78,20 @@ const Home = () => {
           onSubmit={event => {
             event.preventDefault();
             Router.push({
-              pathname: '/user',
+              pathname: "/user",
               query: { id: username }
             });
           }}
         >
           <Octicon icon={MarkGithub} size="large" />
           <label htmlFor="username">OctoDashboard</label>
-          <input name="username" type="text" onChange={handleChange} placeholder="Enter a github user..." />
+          <p>Enter a GitHub username</p>
+          <input
+            name="username"
+            type="text"
+            onChange={handleChange}
+            placeholder="ex. 'nosregor'"
+          />
         </form>
       </StyledContainer>
     </main>
