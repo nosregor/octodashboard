1d0eeef31995a67fc3a6da94f14856ac6d5f93e1
Andrew Rogerson <7705200+nosregor@users.noreply.github.com>
2026-06-18 09:39:22 +0300

    deply visitor count

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {};
+module.exports = {
+  outputFileTracingIncludes: {
+    '/api/visits': ['./node_modules/uncrypto/dist/**/*'],
+  },
+};
